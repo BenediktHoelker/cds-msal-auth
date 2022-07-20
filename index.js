d2d199a6491fc1293dcb0b3c4929f9729c94ce67
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2022-07-20 17:47:06 +0200

    chore: default authenticated user

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ DEBUG?.("[auth] - loading custom auth handler");
 const CDSUser = class extends cds.User {
   is(role) {
     DEBUG?.(`[auth] - ${role}`);
-    return role === "any" || this._roles[role];
+    return role === "any" || role === "authenticated-user" || this._roles[role];
   }
 };
 
@@ -28,13 +28,14 @@ function formatSchema(tenantID) {
  * @param {function} next
  */
 module.exports = (req, res, next) => {
-  const { tenantId, user } = req.session.account || {};
-  DEBUG?.(`[auth] - user defined?${!!user}`);
-  if (user) {
-    req.user = new CDSUser(user);
+  const { tenantId, username } = req.session.account || {};
+  DEBUG?.(`[auth] - user defined?${!!username}`);
+  if (username) {
+    req.user = new CDSUser(username);
     req.user.accessToken = req.session.accessToken;
     req.user.tenant = tenantId;
     req.user.schema = formatSchema(tenantId);
+    req.headers.authentication = true;
     next();
   } else {
     res.status(401).send();
