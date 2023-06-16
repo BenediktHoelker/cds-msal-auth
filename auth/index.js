a37375b55189747e8344ac5dc2f24b7b9cf6e1d8
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-06-16 09:36:17 +0200

    chore: tidy up auth-files

--- auth/index.js
+++ auth/index.js
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
 require("dotenv").config();
 
 const express = require("express");
@@ -31,50 +30,50 @@ function unless(middleware, ...paths) {
 // See: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-node/docs/accounts.md
 async function acquireTokenSilent(req) {
   // Find all accounts
-  // const msalInstance = new msal.ConfidentialClientApplication(msalConfig);
-  // const msalTokenCache = msalInstance.getTokenCache();
+  const msalTokenCache = msalInstance.getTokenCache();
 
   // Account selection logic would go here
-  // const [account] = await msalTokenCache.getAllAccounts();
+  const [account] = await msalTokenCache.getAllAccounts();
 
-  const { account } = req.session || {}; // Select Account code
+  // The MSGraph token is shortlived => Refresh it regularly
+  let forceRefresh = true;
+  if (req.session.timer && Date.now() < req.session.timer + 60000 * 30) {
+    forceRefresh = false;
+  }
+
+  req.session.timer = Date.now();
 
   // Build silent request after account is selected
   const silentRequest = {
     account,
+    forceRefresh,
     scopes: ["User.Read", "Calendars.ReadWrite"],
   };
 
-  return msalInstance.acquireTokenSilent(silentRequest).then((response) => {
-    req.session.accessToken = response.accessToken;
-  });
+  return msalInstance.acquireTokenSilent(silentRequest);
 }
 
 // custom middleware to check auth state
-function redirectToSignin(req, res, next) {
-  if (!req.session.isAuthenticated) {
+async function isAuthenticated(req, res, next) {
+  try {
+    const tokenResponse = await acquireTokenSilent(req, res);
+
+    req.session.accessToken = tokenResponse.accessToken;
+    req.session.idToken = tokenResponse.idToken;
+    req.session.account = tokenResponse.account;
+  } catch (error) {
     res.redirect("/auth/signin"); // redirect to sign-in route
     return;
   }
-  next();
-}
-
-function isAuthenticated(req, res, next) {
-  if (!req.session.isAuthenticated) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
 
   next();
 }
-
 module.exports = function () {
   const router = express.Router();
   router.use(logger("dev"));
   router.use(express.json());
   router.use(cookieParser());
   router.use(express.urlencoded({ extended: false }));
-  // app.use(express.static(path.join(__dirname, "public")));
 
   /**
    * Using express-session middleware for persistent user session. Be sure to
@@ -92,28 +91,11 @@ module.exports = function () {
     })
   );
 
-  router.use("/v2", isAuthenticated, async (req, res, next) => {
-    // TODO: check for server-address with regex: https://blogs.sap.com/2021/10/14/create-authenticated-endpoints-in-cap-that-serve-any-type-of-response/
-    // if (req.path.includes("/timetracking/")) {
-    try {
-      // Acquire Token Silently to be used in MS Graph call
-      // TODO: reconsider performance (atm) each request waits for a refreshed token
-      await acquireTokenSilent(req, res);
-    } catch (err) {
-      req.session.isAuthenticated = false;
-      res.status(401);
-      res.send(err.errorMessage);
-      return;
-    }
-
-    next();
-  });
-
   router.use("/users", usersRouter);
   router.use("/auth", authRouter);
   router.use(
     unless(
-      redirectToSignin,
+      isAuthenticated,
       "/auth/signin",
       "/auth/redirect",
       "/auth/logout",
