b4e5ee5292fadc2dca5a309fc056f3fccb96d996
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-06-24 15:18:59 +0200

    chore: cookie shall expire after one day

--- auth/index.js
+++ auth/index.js
@@ -86,6 +86,7 @@ module.exports = function () {
       resave: false,
       saveUninitialized: true,
       cookie: {
+        maxAge: 86400000,
         sameSite: false,
         secure: false, // set this to true on production
       },
